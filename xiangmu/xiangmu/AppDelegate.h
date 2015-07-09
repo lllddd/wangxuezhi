97a981f2384b5be83829ae2df4af7007ef6333e5
lanou3g <xuezhi08@sina.com>
2015-07-09 09:42:43 +0800

    添加新工程

---
+++ xiangmu/xiangmu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xiangmu
+//
+//  Created by lanou3g on 15/7/9.
+//  Copyright (c) 2015年 王学志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xiangmu/xiangmu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xiangmu
+//
+//  Created by lanou3g on 15/7/9.
+//  Copyright (c) 2015年 王学志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
